d53cbf652741eb80a9d4a892c730161c48ab1b09
Feyza Coşar <mfcosar@hotmail.com>
2023-12-20 12:45:15 +0300

    Basics solution dosyaları GitHub'a eklemek için init yapıldı.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Basics.Models;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index1()
+        {
+            string message = $"Hello world. {DateTime.Now.ToString() }";
+            return View("Index1",message);
+        }
+
+        public ViewResult Index2()
+        {
+            //return View(); --> demiş olsaydık Index2.cshtml arayacaktı View folder'ın altında
+
+            var names = new String[]
+            {
+                "Ahmet","Merhmet","Tahir"
+
+            };
+
+            return View(names);
+
+        }
+
+        public IActionResult Index3()
+        {
+            var list = new List<Employee>();
+            {
+                
+                list.Add(new Employee(){ Id = 1, FirstName = "Ahmet", LastName = "Uygun", Age = 20 });
+                list.Add(new Employee(){ Id = 2, FirstName = "Mehmet", LastName = "Oba", Age = 25 });
+                list.Add(new Employee(){ Id = 3, FirstName = "Yasin", LastName = "Koç", Age = 30 });
+
+                
+            }
+
+            return View("Index3", list);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Basics.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+
+        public string LastName { get; set; } = String.Empty;
+
+        public string FullName => $"{FirstName} {LastName.ToUpper()}";
+
+        public int Age { get; set; } = 0;
+    }
+}
